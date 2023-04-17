7f7e3f689709efc563293dd016d7de19ef9707ac
SantiAtalis <santiago@atalisfunding.com>
2023-04-17 14:43:49 +0200

    create custom styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import theme from './utils/theme'
+import { ChakraProvider, Container } from '@chakra-ui/react'
 
 const App = () => {
   return (
-    <div className='App'>Hi world!</div>
+    <ChakraProvider theme={theme}>
+      <Container maxW='100vw' color='secondary.100'>ehrfuhesurhiu </Container>
+    </ChakraProvider>
   )
 }
 
---
+++ src/utils/theme.js
@@ -0,0 +1,13 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  colors: {
+    primary: {},
+    secondary: {
+      100: '#ffd617'
+    },
+    detail: {}
+  }
+})
+
+export default theme
