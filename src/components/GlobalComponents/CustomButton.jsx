1ce7de495659459c3b9411ba006276189f4f9bb1
SantiAtalis <santiago@atalisfunding.com>
2023-04-18 09:11:06 +0200

    do card prototype

--- src/components/GlobalComponents/CustomButton.jsx
+++ src/components/GlobalComponents/CustomButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Button } from '@chakra-ui/react'
 
 const CustomButton = ({ Icon = null }) => (
-  <Button leftIcon={Icon ?? Icon} bg='secondary.100' color='primary.100' _hover={{ backgroundColor: 'blue', color: 'primary.100' }}>Click me</Button>
+  <Button leftIcon={Icon ?? Icon} bg='secondary.base' color='primary.base' _hover={{ backgroundColor: 'blue', color: 'primary.base' }}>Click me</Button>
 )
 
 export default CustomButton
